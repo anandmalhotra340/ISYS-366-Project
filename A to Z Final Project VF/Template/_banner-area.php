85a46bd66623314375cafbe2a7fc9f4ea15aed7b
Anand Malhotra <102329346+anandmalhotra340@users.noreply.github.com>
2022-05-04 08:46:17 -0400

    5-4-2022

---
+++ A to Z Final Project VF/Template/_banner-area.php
@@ -0,0 +1,15 @@
+<!-- Owl-carousel -->
+<section id="banner-area">
+    <div class="owl-carousel owl-theme">
+        <div class="item">
+            <img src="assets\Castle-Banner.png" alt="Castle-Banner">
+        </div>
+        <div class="item">
+            <img src="assets\Eileen-Banner.png" alt="Skull-Banner">
+        </div>
+        <div class="item">
+            <img src="assets\Skull-Banner.png" alt="Elieen-Banner">
+        </div>
+    </div>
+</section>
+<!-- !Owl-carousel -->
